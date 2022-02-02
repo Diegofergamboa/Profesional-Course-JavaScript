d2f2418f27439429fa4041a60a2b9349b6d7ae97
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-02 15:01:44 -0500

    pause

--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,8 +1,12 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 };
 
 
+MediaPlayer.prototype._initplig
 MediaPlayer.prototype.play = function() {
     this.media.play();
 };
