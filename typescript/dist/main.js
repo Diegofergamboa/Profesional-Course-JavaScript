d6ff20661a3f05f058b6e4a086bfb11d99d2e142
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-05 15:13:45 -0500

    typescript string booleans and numbers

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,2 +1,17 @@
 "use strict";
 console.log('hola platzi world');
+let phone;
+phone = 1;
+// Inferido
+let phoneNumber = 554499;
+phoneNumber = 1234;
+// Se declara el tipo de variable y el valor de la variable.
+let hexa = 0xf00d;
+let bina = 0b1010;
+let octal = 0o744;
+// Boolean
+let ispRO;
+ispRO = true;
+// Strings en TS
+let username = 'Diegofergamboa';
+console.log(username);
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,1 +1,26 @@
 console.log('hola platzi world');
+
+let phone : number;
+phone = 1;
+
+
+// Inferido
+let phoneNumber = 554499;
+phoneNumber = 1234;
+// Se declara el tipo de variable y el valor de la variable.
+let hexa : number = 0xf00d;
+let bina : number = 0b1010;
+let octal : number = 0o744;
+
+// Boolean
+let ispRO : boolean;
+ispRO = true;
+
+// Strings en TS
+// La importancia de TS es que siempre va a permitir el que podamos fijar el valor del tipado que va a tener la variable.
+let username : string = 'Diegofergamboa';
+
+// Backtick o Backquote(``) para colocar expresiones dentro de las variables.
+
+// Template String
+// Uso de back-tick ``
