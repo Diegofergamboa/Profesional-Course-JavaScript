d0652020f3d50d46a4dc85c0f4390ab057adeea8
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-08 15:21:28 -0500

    tipo null de variable

---
+++ typescript/dist/type-null-undefined.js
@@ -0,0 +1,5 @@
+let nullVariable;
+nullVariable = null;
+nullVariable = 'hola'; // nunca se podrá generar otra variable a esta que sea distinto de null
+let otherVariable = null; // Por lo que no se deja de manera explicita el tipo del dato se va a dejar colocar otro tipo de valor.
+otherVariable = 'test';
