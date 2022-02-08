e519f8e3a5bb635e0083dcecc6d85032b9e2534b
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-08 15:25:11 -0500

    null y undefined como subtipos

--- typescript/dist/type-null-undefined.js
+++ typescript/dist/type-null-undefined.js
@@ -1,5 +1,13 @@
-let nullVariable;
+let nullVariable; // Postfija
 nullVariable = null;
 nullVariable = 'hola'; // nunca se podrá generar otra variable a esta que sea distinto de null
 let otherVariable = null; // Por lo que no se deja de manera explicita el tipo del dato se va a dejar colocar otro tipo de valor.
 otherVariable = 'test';
+//Undefined 
+let undefinedVariable = undefined;
+// undefinedVariable = 'string';
+let otherUndefinedVariable = undefined; // Esto se da porque como no está postfijo el tipo de variable que tenemos, podemos asignarle distintos tipos de valores.
+otherUndefinedVariable = 1; // 1
+// Es decir null y undefined pueden usarse como subtipos, tipos y valores de variables.
+// Es decir undefined y null son subtipos o ramas de any.
+// Null y undefined como subtipos.
--- typescript/src/type-null-undefined.ts
+++ typescript/src/type-null-undefined.ts
@@ -1,7 +1,21 @@
-let nullVariable: null;
+let nullVariable: null; // Postfija
 nullVariable = null;
 nullVariable = 'hola'; // nunca se podrá generar otra variable a esta que sea distinto de null
 
 let otherVariable = null; // Por lo que no se deja de manera explicita el tipo del dato se va a dejar colocar otro tipo de valor.
 otherVariable = 'test';
+
+//Undefined 
+let undefinedVariable: undefined = undefined;
+// undefinedVariable = 'string';
+
+
+let otherUndefinedVariable = undefined; // Esto se da porque como no está postfijo el tipo de variable que tenemos, podemos asignarle distintos tipos de valores.
+otherUndefinedVariable = 1 ; // 1
+
+// Es decir null y undefined pueden usarse como subtipos, tipos y valores de variables.
+
+// Es decir undefined y null son subtipos o ramas de any.
+
+// Null y undefined como subtipos.
 
