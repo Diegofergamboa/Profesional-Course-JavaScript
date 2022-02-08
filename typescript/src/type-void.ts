94e3088b67920dbbfaba66b74a952c22199164e4
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-08 14:55:31 -0500

    tipo void estudiado

--- typescript/src/type-void.ts
+++ typescript/src/type-void.ts
@@ -1,2 +1,41 @@
 // El tipo de valor void en typescript por lo general es lo opuesto de any, es "ausencia de valor". 
-// Se sulee usar como retorno de una función y con retorno de una variable.
+// Se suele usar como retorno de una función y con retorno de una variable.
+
+
+//Void.
+
+
+
+//Tipo explicito
+function showFunction(user: any): any { 
+    // El tipo de dato lo da como void porque no detecta el tipo de valor que se tiene.
+    console.log('Userinfo', user.id, user.username, user.firstName);
+    return 'hola'; // Aqui va a mostrar que el tipo de dato va a ser string.
+}
+
+showFunction({
+    id: 1,
+    username: 'Diego',
+    firstName: 'Fernando'
+});
+
+// Tipo inferido.
+
+function showFormattedInfo(user: any) {
+    console.log('User info', `
+        id: ${user.id} ,
+        username: ${user.username} ,
+        firstName: ${user.firstName}
+    `);
+}
+showFormattedInfo({
+    id: 1,
+    username: 'Diego',
+    firstName: 'Fernando'
+});
+
+//Void como tipo de variable,
+
+let unusuable: void;
+unusuable = null;
+unusuable = undefined;
